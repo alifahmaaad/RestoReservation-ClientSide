55f4b5b7826401ee71f6aca122fb563cb3a2d12e
Ahmad Alif Sofian <alif12sofian@gmail.com>
2023-08-24 07:43:23 +0700

    redux toolkit and redux persiist

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,18 @@
+import { useDispatch, useSelector } from "react-redux";
+import { set } from "../redux/slices/dataUserResponse";
+import { useState } from "react";
+
 const Home = () => {
-  return <div className="my-2 flex">Home</div>;
+  const dataSelector = useSelector((state) => state.dataUserResponseRedux);
+  const dispatch = useDispatch();
+  return (
+    <>
+      <div className="my-2 flex" onClick={() => dispatch(set(data))}>
+        Home
+      </div>
+      <button onClick={() => console.log(dataSelector)}>BUTTON</button>
+    </>
+  );
 };
 
 export default Home;
---
+++ src/redux/slices/dataUserResponse.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const dataUserResponse = createSlice({
+  name: "dataUserResponse",
+  initialState: {
+    token: "",
+    dataUser: "",
+  },
+  reducers: {
+    set: (state, action) => {
+      state.token = action.payload.token;
+      state.dataUser = action.payload.dataUser;
+    },
+  },
+});
+export const { set } = dataUserResponse.actions;
+
+export default dataUserResponse.reducer;
