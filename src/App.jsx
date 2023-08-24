7be6c430ca1952862de2cdd8cc0bc396bec1ba74
Ahmad Alif Sofian <alif12sofian@gmail.com>
2023-08-24 17:41:30 +0700

    add login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Restaurant from "./pages/Restaurant";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/restaurant" element={<Restaurant />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   );
