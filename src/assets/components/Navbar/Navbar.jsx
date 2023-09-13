34c8f1ebbb506fb55c2fa3b8fd7b26594340ab0b
Ahmad Alif Sofian <alif12sofian@gmail.com>
2023-09-13 13:50:00 +0700

    add edit profile and resto in dropdown navbar

--- src/assets/components/Navbar/Navbar.jsx
+++ src/assets/components/Navbar/Navbar.jsx
@@ -1,11 +1,16 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../../../redux/slices/dataUserResponse";
+import NavbarMobile from "./NavbarMobile";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
 const Navbar = () => {
   const [navbar, setNavbar] = useState(false);
+  const navigate = useNavigate();
   const dispatch = useDispatch();
-  const { dataUser } = useSelector((state) => state.dataUserResponseRedux);
   const [isOpen, setisOpen] = useState(false);
+  const { dataUser } = useSelector((state) => state.dataUserResponseRedux);
   const changeBackground = () => {
     if (window.scrollY >= 10) {
       setNavbar(true);
@@ -25,7 +30,7 @@ const Navbar = () => {
           " fixed left-0 top-0 flex h-[55px] max-h-[55px] w-full justify-center duration-300"
         }
       >
-        <div className="flex w-[calc(100svw-10px)] items-center justify-between gap-3 md:w-[calc(100svw-100px)]">
+        <div className="flex w-[calc(100svw-10px)] items-center justify-between gap-3 md:w-[80vw]">
           <div className="flex gap-4">
             <a className="flex items-center gap-2" href="/">
               <p className="text-3xl font-bold">
@@ -74,104 +79,7 @@ const Navbar = () => {
               )}
             </ul>
           </div>
-          <div
-            className={
-              (isOpen ? "translate-y-2 " : "") +
-              "group absolute right-0 top-0 z-50 flex flex-col gap-1 p-5 font-bold duration-700 md:hidden"
-            }
-            onClick={() => setisOpen(!isOpen)}
-          >
-            <span
-              className={
-                (isOpen ? "rotate-45 " : "") +
-                "h-[2px] w-4 bg-[#FFB100]  duration-700"
-              }
-            />
-            <span
-              className={
-                (isOpen ? "opacity-0 " : "opacity-100 ") +
-                "h-[2px] w-4 bg-[#FFB100]  duration-700"
-              }
-            />
-            <span
-              className={
-                (isOpen ? "absolute -rotate-45 " : "") +
-                "h-[2px] w-4 bg-[#FFB100]"
-              }
-            />
-          </div>
-          <div
-            className={
-              (isOpen
-                ? "h-full w-screen opacity-100"
-                : "h-full w-0 px-0 opacity-0") +
-              " nav fixed left-0 top-0 z-30 flex flex-col justify-between gap-5 overflow-hidden bg-white p-5 pt-14 font-semibold shadow-lg duration-700"
-            }
-          >
-            <ul className="flex-col items-center gap-5 px-5">
-              <li
-                className={
-                  (window.location.pathname == "/"
-                    ? "opacity-100 after:w-full "
-                    : "opacity-50 after:w-0 ") +
-                  "flex w-fit flex-col  pt-2 duration-500 after:h-1 after:bg-[#FFB100] after:duration-700 after:content-[''] hover:opacity-100 hover:after:w-full"
-                }
-              >
-                <a href="/" className="font-semibold ">
-                  Home
-                </a>
-              </li>
-              <li
-                className={
-                  (window.location.pathname == "/reservation"
-                    ? "opacity-100 after:w-full "
-                    : "opacity-50 after:w-0 ") +
-                  "flex w-fit flex-col  pt-2 duration-500 after:h-1 after:bg-[#FFB100] after:duration-700 after:content-[''] hover:opacity-100 hover:after:w-full"
-                }
-              >
-                <a href="/reservation" className="font-semibold ">
-                  Reservation
-                </a>
-              </li>
-              {dataUser.role == "App_Admin" && (
-                <li
-                  className={
-                    (window.location.pathname == "/user"
-                      ? "opacity-100 after:w-full "
-                      : "opacity-50 after:w-0 ") +
-                    "flex w-fit flex-col  pt-2 duration-500 after:h-1 after:bg-[#FFB100] after:duration-700 after:content-[''] hover:opacity-100 hover:after:w-full"
-                  }
-                >
-                  <a href="/user" className="font-semibold ">
-                    User
-                  </a>
-                </li>
-              )}
-            </ul>
-            <div className="p-5">
-              {dataUser == "" ? (
-                <a
-                  className="flex min-w-[8rem] items-center rounded-full bg-[#ffedc3] px-2 py-1 shadow-md duration-500 hover:scale-110 hover:shadow-lg"
-                  href="/login"
-                >
-                  <p className="w-full text-center text-sm font-semibold md:text-base">
-                    login
-                  </p>
-                </a>
-              ) : (
-                <button
-                  className="flex min-w-[8rem] items-center rounded-full bg-[#ffedc3] px-2 py-1 shadow-md duration-500 hover:scale-110 hover:shadow-lg"
-                  onClick={(e) => {
-                    dispatch(logout());
-                  }}
-                >
-                  <p className="w-full text-center text-sm font-semibold md:text-base">
-                    logout
-                  </p>
-                </button>
-              )}
-            </div>
-          </div>
+          <NavbarMobile />
           <div className="hidden md:flex">
             {dataUser == "" ? (
               <a
@@ -184,15 +92,55 @@ const Navbar = () => {
               </a>
             ) : (
               <button
-                className="flex min-w-[8rem] items-center rounded-full bg-[#ffedc3] px-2 py-1 shadow-md duration-500 hover:scale-110 hover:shadow-lg"
-                onClick={(e) => {
-                  dispatch(logout());
+                onClick={() => {
+                  setisOpen(!isOpen);
                 }}
+                className="flex items-center gap-2 font-sans font-semibold text-[#FFB100] after:translate-x-5 after:translate-y-0.5 after:-rotate-90 after:gap-5 after:content-['\276E']"
               >
-                <p className="w-full text-center text-sm font-semibold md:text-base">
-                  logout
-                </p>
+                <FontAwesomeIcon icon={faUser} />
+                <p className="text-black">{dataUser.username}</p>
               </button>
+            )}
+            {isOpen && (
+              <div className="absolute translate-y-11 rounded-md bg-white p-3 shadow-2xl">
+                <div className="flex flex-col items-start gap-3 px-4">
+                  <button
+                    onClick={() => {
+                      setisOpen(!isOpen);
+                      navigate("/user/update");
+                    }}
+                  >
+                    <p className="w-full text-center text-sm  md:text-base">
+                      Edit Profile
+                    </p>
+                  </button>
+                  <button
+                    onClick={() => {
+                      setisOpen(!isOpen);
+                      navigate("/resto/update");
+                    }}
+                    className={
+                      dataUser.role != "Restaurant_Admin" ? "hidden" : ""
+                    }
+                  >
+                    <p className="w-full text-center text-sm  md:text-base">
+                      Edit Restaurant
+                    </p>
+                  </button>
+                  <button
+                    className="flex min-w-[8rem] items-center rounded-full bg-[#ffedc3] px-2 shadow-md duration-500 hover:scale-110 hover:shadow-lg"
+                    onClick={() => {
+                      dispatch(logout());
+                      setisOpen(!isOpen);
+                      navigate("/login");
+                    }}
+                  >
+                    <p className="w-full text-center text-sm font-semibold md:text-base">
+                      logout
+                    </p>
+                  </button>
+                </div>
+              </div>
             )}
           </div>
         </div>
